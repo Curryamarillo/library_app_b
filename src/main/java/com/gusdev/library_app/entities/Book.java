bad56563ac71af70cfab559c96e5da084e676646
Gustavo Starace <practicodecocina@gmail.com>
2024-05-18 08:58:37 +0200

    feat: adding services

--- src/main/java/com/gusdev/library_app/entities/Book.java
+++ src/main/java/com/gusdev/library_app/entities/Book.java
@@ -15,7 +15,7 @@ import java.util.Set;
 @Table(name = "books")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/com/gusdev/library_app/entities/Loan.java
+++ src/main/java/com/gusdev/library_app/entities/Loan.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Loan {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
