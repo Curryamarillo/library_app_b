ebb794ff01f6736be5af1a67def57d8668ba2ae4
Gustavo Starace <practicodecocina@gmail.com>
2024-06-20 12:55:10 +0200

    fix: data&time

--- src/main/java/com/gusdev/library_app/LibraryAppApplication.java
+++ src/main/java/com/gusdev/library_app/LibraryAppApplication.java
@@ -1,7 +1,5 @@
 package com.gusdev.library_app;
 
-import com.gusdev.library_app.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryAppApplication {
 
-	@Autowired
-	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryAppApplication.class, args);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO books (title, author, isbn, is_available, created_date)
-VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '978-3-16-148410-0', true, CURRENT_TIMESTAMP),
-('To Kill a Mockingbird', 'Harper Lee', '978-3-16-148411-0', true, CURRENT_TIMESTAMP),
+VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '978-3-16-148410-0', false, CURRENT_TIMESTAMP),
+('To Kill a Mockingbird', 'Harper Lee', '978-3-16-148411-0', false, CURRENT_TIMESTAMP),
 ('1984', 'George Orwell', '978-3-16-148412-0', true, CURRENT_TIMESTAMP),
-('Pride and Prejudice', 'Jane Austen', '978-0-14-043528-8', true, CURRENT_TIMESTAMP),
-('The Lord of the Rings: The Fellowship of the Ring', 'J.R.R. Tolkien', '978-0-547-52951-3', true, CURRENT_TIMESTAMP),
+('Pride and Prejudice', 'Jane Austen', '978-0-14-043528-8', false, CURRENT_TIMESTAMP),
+('The Lord of the Rings: The Fellowship of the Ring', 'J.R.R. Tolkien', '978-0-547-52951-3', false, CURRENT_TIMESTAMP),
 ('The Hitchhikers Guide to the Galaxy', 'Douglas Adams', '978-0-345-39180-3', true, CURRENT_TIMESTAMP);
 INSERT INTO users (name, surname, email, is_admin, password)
 VALUES
@@ -30,17 +30,7 @@ VALUES (
   CURRENT_TIMESTAMP
 ),
 (
-  (SELECT id FROM users WHERE email = 'david.miller@example.com'),
-  (SELECT id FROM books WHERE title = 'The Lord of the Rings: The Fellowship of the Ring'),
-  CURRENT_TIMESTAMP
-),
-(
   (SELECT id FROM users WHERE email = 'emily.garcia@example.com'),
   (SELECT id FROM books WHERE title = 'The Hitchhikers Guide to the Galaxy'),
-  CURRENT_TIMESTAMP
-),
-(
-  (SELECT id FROM users WHERE email = 'michael.johnson@example.com'),
-  (SELECT id FROM books WHERE title = '1984'),
   CURRENT_TIMESTAMP
 );
